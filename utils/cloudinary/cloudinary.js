69fd1156a9adc9b86bdb410efacbac9d24307642
ikennaex <ikennaexcel@gmail.com>
2025-10-26 03:41:29 +0100

    feat:watermark added

--- utils/cloudinary/cloudinary.js
+++ utils/cloudinary/cloudinary.js
@@ -1,12 +1,30 @@
 // import { v2 as cloudinary } from "cloudinary";
 const streamifier = require("streamifier");
-const cloudinary = require("../../config/cloudinary")
-
+const cloudinary = require("../../config/cloudinary");
 
 const uploadToCloudinary = async (fileBuffer, folder = "uploads") => {
   return new Promise((resolve, reject) => {
     const uploadStream = cloudinary.uploader.upload_stream(
-      { folder },
+      {
+        folder,
+        transformation: [
+          {
+            overlay: {
+              font_family: "Arial",
+              font_size: 80,
+              font_weight: "bold",
+              text: "www.oscrovilla.com",
+            },
+            color: "#000000", 
+            opacity: 50, 
+            gravity: "center",
+          },
+          
+          { angle: -45 },
+          
+          { flags: "layer_apply" },
+        ],
+      },
       (error, result) => {
         if (error) return reject(error);
         resolve(result);
